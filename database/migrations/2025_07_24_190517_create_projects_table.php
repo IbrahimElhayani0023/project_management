60ec12648a7090cc1d0a605de7494db9abf17207
IbrahimElhayani0023 <ibrahimelh23@gmail.com>
2025-07-24 20:30:54 +0100

    setup database migration

---
+++ database/migrations/2025_07_24_190517_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('workflow_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('star_date');
+            $table->timestamp('end_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2025_07_24_190527_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->enum('status', ['pending', 'todo', 'in progress', 'in review', 'done']);
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('star_date');
+            $table->timestamp('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
